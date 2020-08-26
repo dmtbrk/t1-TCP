8401f1c4d39a35de2a72b52e71855b4953dd703b
Dmytro Brykovets <dmytro.brykovets@gmail.com>
2020-08-26 16:31:10 +0300

    add gracefull shutdown

--- mmtp/server.go
+++ mmtp/server.go
@@ -2,9 +2,12 @@ package mmtp
 
 import (
 	"bufio"
+	"errors"
 	"io"
 	"log"
 	"net"
+	"os"
+	"sync"
 	"time"
 )
 
@@ -12,6 +15,12 @@ type Server struct {
 	Addr        string
 	Handler     Handler
 	IdleTimeout time.Duration
+
+	listener   net.Listener
+	conns      map[net.Conn]bool
+	inShutdown bool
+
+	mu sync.Mutex
 }
 
 func (srv *Server) ListenAndServe() error {
@@ -23,32 +32,33 @@ func (srv *Server) ListenAndServe() error {
 	if err != nil {
 		return err
 	}
-	return srv.Serve(ln)
+	srv.listener = ln
+	return srv.Serve()
 }
 
-func (srv *Server) Serve(ln net.Listener) error {
-	defer ln.Close()
+func (srv *Server) Serve() error {
+	defer srv.Shutdown()
 
-	for {
-		conn, err := ln.Accept()
+	for !srv.inShutdown { // http uses atomicBool
+		conn, err := srv.listener.Accept()
 		if err != nil {
 			log.Println("ERROR: accepting connection:", err)
 			continue
 		}
+		conn = newConn(conn, srv.IdleTimeout)
+		srv.addConn(conn)
 		log.Println("INFO: connection accepted:", conn.RemoteAddr())
 
 		go srv.Handle(conn)
 	}
+	return nil
 }
 
 func (srv *Server) Handle(conn net.Conn) {
 	defer func() {
-		err := conn.Close()
-		if err != nil {
-			log.Println("ERROR: closing connection:", err)
-		} else {
-			log.Println("INFO: connection closed:", conn.RemoteAddr())
-		}
+		conn.Close()
+		srv.deleteConn(conn)
+		log.Println("INFO: connection closed:", conn.RemoteAddr())
 	}()
 
 	br := bufio.NewReader(conn)
@@ -56,7 +66,8 @@ func (srv *Server) Handle(conn net.Conn) {
 	for {
 		msg, _, err := ReadMessage(br)
 		if err != nil {
-			if err == io.EOF {
+			if err == io.EOF || errors.Is(err, os.ErrDeadlineExceeded) {
+				log.Printf("INFO: deadline exceeded: %v", conn.RemoteAddr())
 				break
 			}
 			log.Printf("ERROR: parsing message from %v: %v", conn.RemoteAddr(), err)
@@ -65,6 +76,68 @@ func (srv *Server) Handle(conn net.Conn) {
 
 		srv.Handler.ServeMMTP(mw, msg)
 	}
+}
+
+func (srv *Server) Shutdown() {
+	log.Println("INFO: shutting down...")
+	srv.inShutdown = true // http uses atomicBool
+	srv.listener.Close()
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Printf("INFO: waiting for %d connections to disconnect", len(srv.conns))
+		default:
+			if len(srv.conns) == 0 {
+				return
+			}
+		}
+	}
+}
+
+func (srv *Server) addConn(conn net.Conn) {
+	srv.mu.Lock()
+	defer srv.mu.Unlock()
+
+	if srv.conns == nil {
+		srv.conns = make(map[net.Conn]bool)
+	}
+
+	srv.conns[conn] = true
+}
+
+func (srv *Server) deleteConn(conn net.Conn) {
+	srv.mu.Lock()
+	defer srv.mu.Unlock()
+
+	delete(srv.conns, conn)
+}
+
+type conn struct {
+	net.Conn
+	IdleTimeout time.Duration
+}
+
+func newConn(c net.Conn, idleTimeout time.Duration) *conn {
+	return &conn{Conn: c, IdleTimeout: idleTimeout}
+}
+
+func (c *conn) Write(p []byte) (int, error) {
+	c.updateDeadline()
+	return c.Conn.Write(p)
+}
+
+func (c *conn) Read(p []byte) (int, error) {
+	c.updateDeadline()
+	return c.Conn.Read(p)
+}
+
+func (c *conn) updateDeadline() {
+	t := time.Now().Add(c.IdleTimeout)
+	c.Conn.SetDeadline(t)
 }
 
 type MessageWriter struct {
@@ -91,6 +164,10 @@ func (f HandlerFunc) ServeMMTP(mw *MessageWriter, msg *Message) {
 }
 
 func ListenAndServe(addr string, handler Handler) error {
-	srv := &Server{Addr: addr, Handler: handler}
+	srv := &Server{
+		Addr:        addr,
+		IdleTimeout: 5 * time.Second,
+		Handler:     handler,
+	}
 	return srv.ListenAndServe()
 }
