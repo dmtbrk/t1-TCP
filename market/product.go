7c13ba7c800766c1d27598096fd68e6670d69a4d
Dmytro Brykovets <dmytro.brykovets@gmail.com>
2020-08-27 19:51:29 +0300

    add comment

--- market/product.go
+++ market/product.go
@@ -20,6 +20,7 @@ func (p *Product) String() string {
 	return fmt.Sprintf("Product{ ID: %d, Name: %s, Price: %d }", p.ID, p.Name, p.Price)
 }
 
+// ParseProduct parses json encoded Product.
 func ParseProduct(s string) (*Product, error) {
 	p := &Product{}
 	err := json.Unmarshal([]byte(s), p)
