9cb2c711f420f092587fae58402b6e6164fef56f
Dmytro Brykovets <dmytro.brykovets@gmail.com>
2020-08-27 12:53:03 +0300

    make cmd client keeping connection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/ortymid/t1-tcp/market"
@@ -20,50 +21,58 @@ const (
 )
 
 func main() {
-
-	fmt.Println("Enter a number representing a message type:")
-	// fmt.Println("1 - Request Product")
-	// fmt.Println("2 - Send Product")
-	fmt.Println("101 - Request Product List")
-	// fmt.Println("4 - Send Product List")
-	fmt.Println("102 - Add Product")
-
-	fmt.Print("Message type: ")
-	typ := readInt()
-
-	msg := &mtp.Message{Type: typ}
-
-	switch msg.Type {
-	case MessageProductListRequest:
-		msg.Payload = ""
-	case MessageProductAdd:
-		fmt.Print("\nEnter product name: ")
-		name := readString()
-		fmt.Print("Enter product price: ")
-		price := readInt()
-		product := &market.Product{Name: name, Price: price}
-		pld, err := json.Marshal(product)
-		if err != nil {
-			panic(err)
-		}
-		msg.Payload = string(pld)
-	default:
-		fmt.Println("Unknown message type. Exiting.")
-		os.Exit(0)
-	}
-
 	c, err := mtp.Dial(":8080")
 	if err != nil {
 		panic(err)
 	}
-	c.SendMessage(msg)
+	for {
+		fmt.Println("===========================================")
+		fmt.Println("Enter a number representing a message type:")
+		fmt.Println("101 - Request Product List")
+		fmt.Println("102 - Add Product")
 
-	res, err := c.ReceiveMessage()
-	if err != nil {
-		panic(err)
+		fmt.Print("Message type: ")
+		typ := readInt()
+
+		msg := &mtp.Message{Type: typ}
+
+		switch msg.Type {
+		case MessageProductListRequest:
+			msg.Payload = ""
+		case MessageProductAdd:
+			fmt.Print("\nEnter product name: ")
+			name := readString()
+			fmt.Print("Enter product price: ")
+			price := readInt()
+			product := &market.Product{Name: name, Price: price}
+			pld, err := json.Marshal(product)
+			if err != nil {
+				panic(err)
+			}
+			msg.Payload = string(pld)
+		default:
+			fmt.Println("Unknown message type. Exiting.")
+			os.Exit(0)
+		}
+
+		err = c.SendMessage(msg)
+		if err != nil {
+			fmt.Println("ERROR sending message:", err)
+			continue
+		}
+
+		res, err := c.ReceiveMessage()
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed. Exiting.")
+				os.Exit(0)
+			}
+			fmt.Println("ERROR receiving message:", err)
+			continue
+		}
+		fmt.Println("\nServer response:")
+		fmt.Println(res)
 	}
-	fmt.Println("\nServer response:")
-	fmt.Println(res)
 }
 
 func readInt() int {
